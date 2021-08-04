3fb0534fc02d54f037636699a8b5c450a3b1a492
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-08-04 13:23:19 +0200

    Nord Coders : my-app : composant et class

--- 01-Nord Coders/mon-app/src/App.js
+++ 01-Nord Coders/mon-app/src/App.js
@@ -1,7 +1,16 @@
-import './css/App.css';
+import React from 'react';
+// function App({title}){
+//     return <h1 className="text-center text-blue-500 text-4xl">{title}</h1>;
+// }
 
-function App({title}){
-    return <h1 className="heading">{title}</h1>;
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { title: 'coucou les amis!' };
+    }
+    render () {
+        return <h1 className="text-center text-blue-500 text-4xl">{this.state.title}</h1>;
+    }
 }
-
 export default App;
