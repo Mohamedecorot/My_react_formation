fd52e92242e0a0b9a67346af417747b289dc9da9
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-08-05 14:49:02 +0200

    Nord Coders : my-app : Passage au component basée sur fonction (useState Hook d’état)

--- 01-Nord Coders/mon-app/src/App.js
+++ 01-Nord Coders/mon-app/src/App.js
@@ -1,38 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Picture from './components/Picture';
-// function App({title}){
-//     return <h1 className="text-center text-blue-500 text-4xl">{title}</h1>;
-// }
 
-class App extends React.Component {
+function App(){
+    //hook
+    const [title, setTitle] = useState('coucou les amis');
+    const [show, setShow] = useState(false);
 
-    constructor(props) {
-        super(props);
-        this.state = { show: false, title: 'coucou les amis' };
+    function handleClick() {
+        setShow(!show);
     }
 
-    componentDidMount() {
-        //this.setState({title: 'le composant a bien été monté'});
-    }
-
-    componentDidUpdate() {
-        console.log('composant mis à jour');
-    }
-    render () {
-        return <div>
-            <h1 className="text-4xl text-purple-700 my-5">{ this.state.title }</h1>
+    return (
+        <div>
+            <h1 className="text-4xl text-purple-700 my-5">{ title }</h1>
             <button className="bg-purple-900 text-white rounded py-2 px-3"
-                    onClick={() => this.setState( {show: !this.state.show}) }>
+                    onClick={ handleClick }>
                 click
             </button>
 
-            {
-                this.state.show ?
-                <Picture />
-                : null
-            }
-
-        </div>;
-    }
+            { show ? <Picture /> : null }
+        </div>
+    );
 }
+
+// class App extends React.Component {
+
+//     constructor(props) {
+//         super(props);
+//         this.state = { show: false, title: 'coucou les amis' };
+//     }
+
+//     componentDidMount() {
+//         //this.setState({title: 'le composant a bien été monté'});
+//     }
+
+//     componentDidUpdate() {
+//         console.log('composant mis à jour');
+//     }
+//     render () {
+//         return <div>
+//             <h1 className="text-4xl text-purple-700 my-5">{ this.state.title }</h1>
+//             <button className="bg-purple-900 text-white rounded py-2 px-3"
+//                     onClick={() => this.setState( {show: !this.state.show}) }>
+//                 click
+//             </button>
+
+//             {
+//                 this.state.show ?
+//                 <Picture />
+//                 : null
+//             }
+
+//         </div>;
+//     }
+// }
 export default App;
