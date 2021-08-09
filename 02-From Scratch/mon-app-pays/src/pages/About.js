a9a008e1047babdd36cf4ec9319ed3e1fb2ec348
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-08-09 15:12:19 +0200

    From Scratch: mon_app_pays : Router

---
+++ 02-From Scratch/mon-app-pays/src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>A propos</h1>
+            <br/>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat, ex? Voluptas incidunt aliquid consequuntur dolor fuga id similique hic iure animi, totam ducimus ut. Quisquam iure dolorem, ratione at dignissimos fugit laudantium sequi sunt velit quasi reprehenderit aspernatur consequatur? Optio cumque nisi amet maiores, minus omnis eum est laboriosam explicabo!</p>
+            <br/>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, quisquam molestiae? Numquam quidem ratione tempore exercitationem facere dolores obcaecati aspernatur quod quos fuga, eius fugit vel possimus ullam quaerat ducimus nemo optio praesentium odit? Iste itaque maxime eaque corrupti vero ab laboriosam repellendus deserunt aliquam illum ipsa doloribus laborum optio, porro consequuntur accusamus eum a sapiente, culpa deleniti! Autem mollitia aliquam, architecto eligendi illo sint ex rem, voluptate iste, officiis optio saepe distinctio veritatis quam praesentium alias magnam laudantium illum quasi exercitationem. Modi natus laudantium aliquid. Ipsum sit, nam adipisci enim accusantium blanditiis, nulla laudantium non distinctio officiis, doloribus vero.</p>
+        </div>
+    );
+};
+
+export default About;
