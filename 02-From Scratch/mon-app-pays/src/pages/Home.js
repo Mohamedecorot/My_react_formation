0d8fedcb69857d513479f25e040e84f22a79317f
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-08-11 05:21:07 +0200

    From Scratch: mon_app_pays : Les hooks (useState)

--- 02-From Scratch/mon-app-pays/src/pages/Home.js
+++ 02-From Scratch/mon-app-pays/src/pages/Home.js
@@ -1,12 +1,13 @@
 import Navigation from "../components/Navigation";
 import Logo from "../components/Logo";
+import Countries from "../components/Countries";
 
 const Home= () => {
     return (
         <div className="hom">
             <Navigation />
             <Logo />
-            <h1>Accueil</h1>
+            <Countries />
         </div>
     )
 }
