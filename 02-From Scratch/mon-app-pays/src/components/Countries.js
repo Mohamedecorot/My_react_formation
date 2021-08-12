e710418f04afef10aa8c1ef0bf351d0e030a8d8e
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-08-12 19:01:48 +0200

    From Scratch: mon_app_pays : API & Axios

--- 02-From Scratch/mon-app-pays/src/components/Countries.js
+++ 02-From Scratch/mon-app-pays/src/components/Countries.js
@@ -1,11 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 
 const Countries = () => {
     const [data, setData] = useState([]);
 
-    return (
-        <div>
+    useEffect(() => {
+        Axios.get('https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag')
+        .then((res) => setData(res.data));
+    }, [])
 
+    return (
+        <div className="countries">
+            <ul className="countries-list">
+                {data.map((country) => (
+                    <li>{country.name}</li>
+                ))}
+            </ul>
         </div>
     );
 };
