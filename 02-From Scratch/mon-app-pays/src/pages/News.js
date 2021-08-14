30a8bb788ecee24a7b11865d8bb6fe69a14a9ee9
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-08-14 15:12:19 +0200

    From Scratch: mon_app_pays : create [post] (crud)

--- 02-From Scratch/mon-app-pays/src/pages/News.js
+++ 02-From Scratch/mon-app-pays/src/pages/News.js
@@ -5,16 +5,34 @@ import axios from 'axios';
 import Article from '../components/Article';
 
 const News = () => {
-    const [newsData, setNewsData] = useState([])
+    const [newsData, setNewsData] = useState([]);
+    const [author, setAuthor] = useState("");
+    const [content, setContent] = useState("");
 
     useEffect(() => {
         getData();
-    });
+    }, []);
     const getData = () => {
         axios
           .get("http://localhost:3003/articles")
           .then((res) => setNewsData(res.data));
       };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        axios
+        .post("http://localhost:3003/articles", {
+            author,
+            content,
+            date: Date.now(),
+        })
+        .then(() => {
+            setAuthor("");
+            setContent("");
+            getData();
+        });
+    };
 
     return (
         <div className="news-container">
@@ -22,14 +40,16 @@ const News = () => {
             <Logo />
             <h1>News</h1>
 
-            <form>
-                <input type="text" placeholder="Nom"/>
-                <textarea placeholder="Message"></textarea>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <input type="text" onChange={(e) => setAuthor(e.target.value)} placeholder="Nom" value={author}/>
+                <textarea onChange={(e) => setContent(e.target.value)} placeholder="Message" value={content}></textarea>
                 <input type="submit" value="Envoyer" />
             </form>
             <ul>
-                {newsData.map((article) => (
-                <Article key={article.id} article={article}/>
+                {newsData
+                    .sort((a, b) => b.date - a.date)
+                    .map((article) => (
+                    <Article key={article.id} article={article} />
                 ))}
             </ul>
         </div>
