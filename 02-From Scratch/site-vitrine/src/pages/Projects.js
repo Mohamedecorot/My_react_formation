1e71d8dc56930a71814162f5ad1314f495d624d4
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2021-08-18 12:44:56 +0200

    From Scratch: site-vitrine : Router

---
+++ 02-From Scratch/site-vitrine/src/pages/Projects.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export const Project1 = () => {
+  return (
+    <main>
+      <div className="project">
+        projet 1
+      </div>
+    </main>
+  );
+};
+
+export const Project2 = () => {
+  return (
+    <main>
+      <div className="project">
+        projet 2
+      </div>
+    </main>
+  );
+};
+
+export const Project3 = () => {
+  return (
+    <main>
+      <div className="project">
+        projet 3
+      </div>
+    </main>
+  );
+};
+
+export const Project4 = () => {
+  return (
+    <main>
+      <div className="project">
+        projet 4
+      </div>
+    </main>
+  );
+};
