d696a56a8363ec6e1accd74adfd236efc41ae545
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2021-09-11 10:26:35 +0200

    Lior Chamla: mon app client : Modifier le state

--- 04- LiorChamla/mon-app-client/src/index.js
+++ 04- LiorChamla/mon-app-client/src/index.js
@@ -11,19 +11,28 @@ class App extends React.Component {
     ]
   };
 
-  handleClick = () => {
-    console.log(this.state);
-  }
+  // handleClick = () => {
+  //   const clients = this.state.clients.slice();
+  //   clients.push({id: 4, nom: "Papa"});
 
+  //   this.setState({clients: clients});
+  // }
+
+  handleDelete = (id) => {
+    const clients = this.state.clients.slice();
+    const index = clients.findIndex(client => client.id === id);
+
+    clients.splice(index, 1);
+    this.setState({clients: clients});
+  }
   render() {
   const title = "Liste des clients";
 
   return (
     <div>
       <h1>{title}</h1>
-      <button onClick={this.handleClick}>Click Me</button>
       <ul>
-        {this.state.clients.map(client => (<li key={client.id}>{client.nom} <button>X</button></li>))}
+        {this.state.clients.map(client => (<li key={client.id}>{client.nom} <button onClick={() => this.handleDelete(client.id)}>X</button></li>))}
       </ul>
       <form>
         <input type="text" placeholder="Ajouter un client"/>
