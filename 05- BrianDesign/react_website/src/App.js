2420251c4d6c9580c77f3df6684cb50abcabe0ec
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2021-09-17 13:58:48 +0200

    Brian Design: react website : Creation de la navbar

--- 05- BrianDesign/react_website/src/App.js
+++ 05- BrianDesign/react_website/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Navbar/>
+    </Router>
   );
 }
 
