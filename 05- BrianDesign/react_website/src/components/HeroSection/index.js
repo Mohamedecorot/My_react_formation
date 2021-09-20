b68660c6a8daa5a0bb305826ef45824fd7ed5518
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2021-09-20 14:33:08 +0200

    Brian Design : React Website: Creation de la section Hero

---
+++ 05- BrianDesign/react_website/src/components/HeroSection/index.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import video from '../../videos/video.mp4';
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP,HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElements';
+
+const HeroSection = () => {
+    const {hover, setHover} = useState(false);
+
+    const onHover = () => {
+        setHover(!hover);
+    }
+    return (
+        <HeroContainer>
+            <HeroBg>
+                <VideoBg autoPlay loop muted src={video} type='video/mp4' />
+            </HeroBg>
+            <HeroContent>
+                <HeroH1>Virtual Banking Made Easy</HeroH1>
+                <HeroP>
+                    Sign up for a new account today and receive 250$ in creadit towards your next payment.
+                </HeroP>
+                <HeroBtnWrapper>
+                    <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover}>
+                        Get Started {hover ? <ArrowForward /> : <ArrowRight/>}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
+        </HeroContainer>
+    )
+}
+
+export default HeroSection
