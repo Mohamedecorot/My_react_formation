742f31bde9c9fd39ed831a8f5f09e9a35222715f
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2021-09-21 13:58:07 +0200

    Brian Design : React Website: Creation de l'element Button

--- 05- BrianDesign/react_website/README.md
+++ 05- BrianDesign/react_website/README.md
@@ -1,1 +1,3 @@
 https://www.youtube.com/watch?v=Nl54MJDR2p8
+
+https://undraw.co/illustrations
--- 05- BrianDesign/react_website/src/components/HeroSection/index.js
+++ 05- BrianDesign/react_website/src/components/HeroSection/index.js
@@ -1,13 +1,15 @@
 import React, {useState} from 'react';
 import video from '../../videos/video.mp4';
+import { Button } from '../ButtonElement';
 import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP,HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElements';
 
 const HeroSection = () => {
-    const {hover, setHover} = useState(false);
+    const [hover, setHover] = useState(false);
 
     const onHover = () => {
         setHover(!hover);
-    }
+    };
+
     return (
         <HeroContainer>
             <HeroBg>
@@ -19,7 +21,7 @@ const HeroSection = () => {
                     Sign up for a new account today and receive 250$ in creadit towards your next payment.
                 </HeroP>
                 <HeroBtnWrapper>
-                    <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover}>
+                    <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover} primary="true" dark="true">
                         Get Started {hover ? <ArrowForward /> : <ArrowRight/>}
                     </Button>
                 </HeroBtnWrapper>
