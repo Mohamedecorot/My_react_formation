d4427dee9a32c1852ba5f249368ba312e530b0a3
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2021-09-23 16:22:50 +0200

    Brian Design : React Website: Ajout de la data

--- 05- BrianDesign/react_website/src/components/InfoSection/index.js
+++ 05- BrianDesign/react_website/src/components/InfoSection/index.js
@@ -2,25 +2,25 @@ import React from 'react'
 import { Button } from '../ButtonElement'
 import { InfoContainer, InfoWrapper, InfoRow, Column1, Column2, TextWrapper, TopLine, Heading, SubTitle, BtnWrap, ImgWrap, Img } from './InfoElements'
 
-const InfoSection = () => {
+const InfoSection = ({lightBg, id, imageStart, topline, lightText, headline, darkText, description, buttonLabel, img, alt}) => {
     return (
         <div>
-            <InfoContainer>
+            <InfoContainer lightBg={lightBg} id={id}>
                 <InfoWrapper>
-                    <InfoRow>
+                    <InfoRow imageStart={imageStart}>
                         <Column1>
                             <TextWrapper>
-                                <TopLine>Topline</TopLine>
-                                <Heading>Heading</Heading>
-                                <SubTitle>Subtile</SubTitle>
+                                <TopLine lightText={lightText}>{topline}</TopLine>
+                                <Heading>{headline}</Heading>
+                                <SubTitle darkText={darkText}>{description}</SubTitle>
                                 <BtnWrap>
-                                    <Button to="home" />
+                                    <Button to="home">{buttonLabel}</Button>
                                 </BtnWrap>
                             </TextWrapper>
                         </Column1>
                         <Column2>
                             <ImgWrap>
-                                <Img />
+                                <Img src={img} alt={alt}/>
                             </ImgWrap>
                         </Column2>
                     </InfoRow>
