43e9d483d79dcaa2d362acedf1f9ebb9d6f61d10
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2021-09-30 11:27:57 +0200

    Brian Design : React Website : Creation du component Sign In

--- 05- BrianDesign/react_website/src/pages/signin.js
+++ 05- BrianDesign/react_website/src/pages/signin.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import SignIn from '../components/SignIn'
 
 const SigninPage = () => {
     return (
-        <div>
-            <h1>Sign in page</h1>
-        </div>
+        <Fragment>
+            <SignIn/>
+        </Fragment>
     )
 }
 
