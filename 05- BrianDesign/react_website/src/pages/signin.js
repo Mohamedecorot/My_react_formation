7c80e2ed89101c8517bbc5e391cacf56ef7a3c49
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2021-10-01 09:26:45 +0200

    Brian Design : React Website : Scroll en haut lors du changement de page

--- 05- BrianDesign/react_website/src/pages/signin.js
+++ 05- BrianDesign/react_website/src/pages/signin.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react'
+import ScrollToTop from '../components/ScrollToTop'
 import SignIn from '../components/SignIn'
 
 const SigninPage = () => {
     return (
         <Fragment>
+            <ScrollToTop/>
             <SignIn/>
         </Fragment>
     )
