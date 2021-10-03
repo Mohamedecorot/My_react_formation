f612fb837c54102d52e8af292a9c148916b05c5c
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2021-10-03 08:03:38 +0200

    Brian Design : Todolist : création du composent TodoList

--- 05- BrianDesign/todolist/src/components/TodoForm.js
+++ 05- BrianDesign/todolist/src/components/TodoForm.js
@@ -10,13 +10,13 @@ function TodoForm(props) {
     const handleSubmit = e => {
         e.preventDefault();
 
-        // props.onSubmit({
-        //     id: Math.floor(Math.random() * 1000 ),
-        //     text: input
-        // });
-    setInput('');
-    };
+        props.onSubmit({
+            id: Math.floor(Math.random() * 1000 ),
+            text: input
+        });
 
+        setInput('');
+    };
 
 
     return (
