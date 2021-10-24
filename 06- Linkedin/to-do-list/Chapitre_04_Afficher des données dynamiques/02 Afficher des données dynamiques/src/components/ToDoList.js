040e7ecebbcdc211dd5d2e8850e4068eaf180d6d
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2021-10-24 16:03:45 +0200

    Linkedin: Chapitre_04_Afficher des données dynamiques : Afficher des données dynamiques

---
+++ 06- Linkedin/to-do-list/Chapitre_04_Afficher des données dynamiques/02 Afficher des données dynamiques/src/components/ToDoList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ToDo from './ToDo'
+
+const ToDoList = ({tasks}) => (
+    <React.Fragment>
+        <h1 className="m-3">Liste de tâches</h1>
+        <ul className="list-group m-3">
+            {
+                tasks.map((task) => <ToDo task={task} key={task.id} />)
+            }
+        </ul>
+    </React.Fragment>
+)
+
+export default ToDoList
