192891d4c5097577713cfac34f98afa2158a39f9
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2022-08-31 08:39:51 +0200

    07- react-pokemon-app : 04-Les Props

---
+++ 07- react-pokemon-app/src/components/pokemon-card.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './pokemon-card.css';
+
+const PokemonCard = ({pokemon, borderColor = '#009688'}) => {
+
+  const [color, setColor] = useState();
+
+  const showBorder = () => {
+    setColor(borderColor);
+  }
+
+  const hideBorder = () => {
+    setColor('#f5f5f5'); //On remet la bordure en gris
+  }
+
+  return (
+    <div className="col s6 m4" onMouseEnter={showBorder} onMouseLeave={hideBorder}>
+      <div className="card horizontal" style={{ borderColor: color}}>
+        <div className="card-image">
+          <img src={pokemon.picture} alt={pokemon.name}/>
+        </div>
+        <div className="card-stacked">
+          <div className="card-content">
+            <p>{pokemon.name}</p>
+            <p><small>{pokemon.created.toString()}</small></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PokemonCard;
