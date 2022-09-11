37cf3b22a269d0d25589d060a0bf29b3079374f8
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2022-09-11 07:34:22 +0200

    02-From Scratch : MERN-réseau social : back-end : 06_Follow et unFollow

--- 02-From Scratch/MERN-réseau social/back-end/controllers/user.controller.js
+++ 02-From Scratch/MERN-réseau social/back-end/controllers/user.controller.js
@@ -49,4 +49,65 @@ module.exports.deleteUser = async (req, res) => {
     } catch (err) {
         return res.status(500).json({ message: err });
     }
-}
+};
+
+module.exports.follow = async (req, res) => {
+    if (!ObjectID.isValid(req.params.id) || !ObjectID.isValid(req.body.idToFollow))
+    return res.status(400).send("ID unknown : " + req.params.id);
+
+    try {
+        // add to the follower list
+        await UserModel.findByIdAndUpdate(
+            req.params.id,
+            { $addToSet: { following: req.body.idToFollow } },
+            { new: true, upsert: true },
+            (err, docs) => {
+                if (!err) return res.status(201).json(docs);
+                else return res.status(400).json(err);
+            }
+        );
+        // add to following list
+        await UserModel.findByIdAndUpdate(
+            req.body.idToFollow,
+            { $addToSet: { followers: req.params.id } },
+            { new: true, upsert: true },
+            (err, docs) => {
+                // if (!err) res.send(201).json(docs);
+                if (err) return res.status(400).json(err);
+            }
+        )
+    } catch (err) {
+        return res.status(500).json({ message: err });
+    }
+};
+
+module.exports.unfollow = async (req, res) => {
+    if (!ObjectID.isValid(req.params.id) || !ObjectID.isValid(req.body.idToUnFollow))
+    return res.status(400).send("ID unknown : " + req.params.id);
+
+    try {
+        // remove to the follower list
+        await UserModel.findByIdAndUpdate(
+            req.params.id,
+            { $pull: { following: req.body.idToUnFollow } },
+            { new: true, upsert: true },
+            (err, docs) => {
+                if (!err) res.send(201).json(docs);
+                else return res.status(400).json(err);
+            }
+        );
+        // remove to following list
+        await UserModel.findByIdAndUpdate(
+            req.body.idToUnFollow,
+            { $pull: { followers: req.params.id } },
+            { new: true, upsert: true },
+            (err, docs) => {
+                // if (!err) res.send(201).json(docs);
+                if (err) return res.status(400).json(err);
+            }
+        )
+    } catch (err) {
+        return res.status(500).json({ message: err });
+    }
+};
+
--- 02-From Scratch/MERN-réseau social/back-end/models/user.model.js
+++ 02-From Scratch/MERN-réseau social/back-end/models/user.model.js
@@ -1,60 +1,53 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
-const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema(
-    {
-        pseudo: {
-            type: String,
-            required: true,
-            minLength: 3,
-            maxLength: 55,
-            unique: true,
-            trim: true
-        },
-        email: {
-            type: String,
-            required: true,
-            validate: [isEmail],
-            lowercase: true,
-            trim: true
-        },
-        password: {
-            type: String,
-            required: true,
-            maxLength: 1024
-        },
-        picture: {
-            type: String,
-            default: "./uploads/profil/random-user.png"
-        },
-        bio: {
-            type: String,
-            maxLength: 1024
-        },
-        followers: {
-            type: [String]
-        },
-        following: {
-            type: [String]
-        },
-        likes: {
-            type: [String]
-        }
+  {
+    pseudo: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 55,
+      unique: true,
+      trim: true
     },
-    {
-        timestamps: true,
+    email: {
+      type: String,
+      required: true,
+      validate: [isEmail],
+      lowercase: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      max: 1024,
+      minlength: 6
+    },
+    picture: {
+      type: String,
+      default: "./uploads/profil/random-user.png"
+    },
+    bio :{
+      type: String,
+      max: 1024,
+    },
+    followers: {
+      type: [String]
+    },
+    following: {
+      type: [String]
+    },
+    likes: {
+      type: [String]
     }
+  },
+  {
+    timestamps: true,
+  }
 );
 
-// play function before save into display: 'block',
-userSchema.pre("save", async function(next) {
-    const salt = await bcrypt.genSalt();
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-})
-
-
-const UserModel = mongoose.model('user', userSchema);
+const UserModel = mongoose.model("user", userSchema);
 
 module.exports = UserModel;
