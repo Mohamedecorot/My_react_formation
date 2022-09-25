a0b358ca8a9beb84b5f41671027ee66c210572cd
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2022-09-25 22:47:51 +0200

    02-From Scratch : MERN-réseau social : front-end : 07-ajout de redux et récupération du pseudo de l'utilsateur dans la bdd

--- 02-From Scratch/MERN-réseau social/front-end/src/index.js
+++ 02-From Scratch/MERN-réseau social/front-end/src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './styles/index.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./styles/index.scss";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// dev tools
+import { composeWithDevTools } from "redux-devtools-extension";
+
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
 );
 
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
