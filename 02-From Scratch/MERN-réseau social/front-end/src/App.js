6b166379bbdfd68b33050c6e06da65d313e3827b
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2022-09-26 08:59:06 +0200

    02-From Scratch : MERN-réseau social : front-end : 07-ajout de redux et récupération du pseudo de l'utilsateur dans la bdd

--- 02-From Scratch/MERN-réseau social/front-end/src/App.js
+++ 02-From Scratch/MERN-réseau social/front-end/src/App.js
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from "react";
 import Routes from "./components/Routes";
 import { UidContext } from "./components/AppContext";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { getUser } from "./actions/user.actions";
 
 const App = () => {
   const [uid, setUid] = useState(null);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchToken = async () => {
@@ -20,7 +23,8 @@ const App = () => {
     };
     fetchToken();
 
-  }, [uid]);
+    if (uid) dispatch(getUser(uid));
+  }, [uid, dispatch]);
 
   return (
     <UidContext.Provider value={uid}>
