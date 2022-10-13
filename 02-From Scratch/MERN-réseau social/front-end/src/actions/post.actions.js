8c084910d7dc645fd1a80f3d147f5fda666ad292
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2022-10-13 17:23:30 +0200

    02-From Scratch : MERN-réseau social : front-end : 25-ajout du trending

--- 02-From Scratch/MERN-réseau social/front-end/src/actions/post.actions.js
+++ 02-From Scratch/MERN-réseau social/front-end/src/actions/post.actions.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 // posts
 export const GET_POSTS = "GET_POSTS";
+export const GET_ALL_POSTS = "GET_ALL_POSTS";
 export const ADD_POST = "ADD_POST";
 
 export const LIKE_POST = "LIKE_POST";
@@ -14,9 +15,11 @@ export const ADD_COMMENT = "ADD_COMMENT";
 export const EDIT_COMMENT = "EDIT_COMMENT";
 export const DELETE_COMMENT = "DELETE_COMMENT";
 
-
 // errors
 export const GET_POST_ERRORS = "GET_POST_ERRORS";
+
+// trends
+export const GET_TRENDS = "GET_TRENDS";
 
 export const getPosts = (num) => {
   return (dispatch) => {
@@ -25,6 +28,7 @@ export const getPosts = (num) => {
       .then((res) => {
         const array = res.data.slice(0, num);
         dispatch({ type: GET_POSTS, payload: array });
+        dispatch({ type: GET_ALL_POSTS, payload: res.data });
       })
       .catch((err) => console.log(err));
   };
@@ -139,5 +143,11 @@ export const deleteComment = (postId, commentId) => {
         dispatch({ type: DELETE_COMMENT, payload: { postId, commentId } });
       })
       .catch((err) => console.log(err));
+  };
+};
+
+export const getTrends = (sortedArray) => {
+  return (dispatch) => {
+    dispatch({ type: GET_TRENDS, payload: sortedArray });
   };
 };
