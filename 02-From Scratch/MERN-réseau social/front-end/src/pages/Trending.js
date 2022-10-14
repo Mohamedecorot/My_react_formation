f052e67373e5d7eb088c1145436ba4934ee7ec32
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2022-10-14 18:23:44 +0200

    02-From Scratch : MERN-réseau social : front-end : 26-ajout des suggestions d'amis

--- 02-From Scratch/MERN-réseau social/front-end/src/pages/Trending.js
+++ 02-From Scratch/MERN-réseau social/front-end/src/pages/Trending.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useSelector } from "react-redux";
+import { UidContext } from "../components/AppContext";
 import LeftNav from '../components/LeftNav';
 import { isEmpty } from "../components/Utils";
 import Card from "../components/Post/Card";
 import Trends from "../components/Trends";
+import FriendsHint from "../components/Profil/FriendsHint";
 
 const Trending = () => {
+  const uid = useContext(UidContext);
   const trendList = useSelector((state) => state.trendingReducer);
 
   return <div className="trending-page">
@@ -18,6 +21,7 @@ const Trending = () => {
     <div className="right-side">
       <div className="right-side-container">
         <Trends />
+        {uid && <FriendsHint />}
       </div>
     </div>
   </div>;
