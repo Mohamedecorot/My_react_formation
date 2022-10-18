02ab79425d35f39f52358608bb5311a10ec2bf3f
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2022-10-18 12:09:57 +0200

    08- App-for-beginner : 03-Reglage des attributs

--- 08- App-for-beginner/src/components/counter.jsx
+++ 08- App-for-beginner/src/components/counter.jsx
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  render() {
-    return <h1>Hello World</h1>;
-  }
+    state = {
+        count: 0,
+        imageUrl: 'https://picsum.photos/200',
+    };
+
+    styles = {
+        fontSize: 10,
+        fontWeight: "bold"
+    };
+
+    render() {
+        return (
+        <>
+            <img src={this.state.imageUrl} alt="" />
+            <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+            <button style={{ fontSize: 16}} className="btn btn-secondary btn-sm">Increment</button>
+        </>
+        );
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
+    }
 }
 
 export default Counter;
