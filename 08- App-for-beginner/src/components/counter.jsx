008e3d2a32a0586742e2f4dace7c6bbbe31af7e7
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2022-10-19 21:00:28 +0200

    08- App-for-beginner : 04-Rendre la classes dynamic

--- 08- App-for-beginner/src/components/counter.jsx
+++ 08- App-for-beginner/src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
     state = {
-        count: 0,
+        count: 1,
         imageUrl: 'https://picsum.photos/200',
     };
 
@@ -14,11 +14,17 @@ class Counter extends Component {
     render() {
         return (
         <>
-            <img src={this.state.imageUrl} alt="" />
-            <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+            <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button style={{ fontSize: 16}} className="btn btn-secondary btn-sm">Increment</button>
+            <img src={this.state.imageUrl} alt="" />
         </>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
     }
 
     formatCount() {
