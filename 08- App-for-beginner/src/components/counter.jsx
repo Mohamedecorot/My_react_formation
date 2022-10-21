06004b27b9b4c2d0913d841179cf82a9c3c78ba4
Mohamedecorot <60646768+Mohamedecorot@users.noreply.github.com>
2022-10-21 09:01:34 +0200

    08- App-for-beginner : 06-Condition

--- 08- App-for-beginner/src/components/counter.jsx
+++ 08- App-for-beginner/src/components/counter.jsx
@@ -12,14 +12,18 @@ class Counter extends Component {
         fontWeight: "bold"
     };
 
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>Il n'y a pas de tag</p>
+        return <ul> { this.state.tags.map(tag => <li key={tag}>{tag}</li>)} </ul>
+    }
+
     render() {
         return (
         <>
             <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button style={{ fontSize: 16}} className="btn btn-secondary btn-sm">Increment</button>
-            <ul>
-                { this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-            </ul>
+            { this.state.tags.length === 0 && "svp créer un nouveau tag"}
+            { this.renderTags() }
             <img src={this.state.imageUrl} alt="" />
         </>
         );
